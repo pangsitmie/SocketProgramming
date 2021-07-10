27e031d55775e2cf013ad023d7f410bdc3bfa0f6
pangsitmie <pangsitmie@github.com>
2021-07-10 17:55:02 +0800

    sctp python

--- Python Socket/TCP/client.py
+++ Python Socket/TCP/client.py
@@ -5,7 +5,7 @@ import socket
 HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT);
+ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE ="!DISCONNECTED"
 
